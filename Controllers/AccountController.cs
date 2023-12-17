efeb2b9683da7b09852c6c9924e0e3d5f220038a
Maksym <max.sheremeta228@gmail.com>
2023-12-17 23:24:05 +0200

    Add authentacation

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,24 +1,69 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using TaskAuthenticationAuthorization.Models;
 using TaskAuthenticationAuthorization.Models.ViewModels;
 
 namespace TaskAuthenticationAuthorization.Controllers
 {
     public class AccountController : Controller
     {
-        //private readonly 
-        public IActionResult Index()
+        private readonly UserContext _userContext;
+        public AccountController(UserContext context)
         {
-            return View();
+            _userContext = context;
         }
+
         [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
-        //public async Task<IActionResult> Register(RegisterViewModel model)
-        //{
-        //    return View();
-        //}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userContext.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+
+                if (user == null)
+                {
+                    _userContext.Users.Add(new User { Email = model.Email, Password = model.Password });
+                    await _userContext.SaveChangesAsync();
+
+                    await Authenticate(model.Email);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Incorrect login and(or) password");
+                }
+            }
+            return View(model);
+        }
+
+        public async Task Authenticate(string userEmail)
+        {
+            //create one claim
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userEmail)
+            };
+
+            // create claim Identiti object
+            ClaimsIdentity Id = new ClaimsIdentity(claims, "ApplicationCookies"
+                ,ClaimsIdentity.DefaultNameClaimType
+                ,ClaimsIdentity.DefaultRoleClaimType);
+
+            //setting authenticational cookies
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(Id));
+        }
     }
 }
