9b87a545ff1e81df9447e15e75c05cc28775bfa9
Maksym <max.sheremeta228@gmail.com>
2023-12-17 23:37:38 +0200

    Add Role to User

--- Models/UserContext.cs
+++ Models/UserContext.cs
@@ -10,11 +10,21 @@ namespace TaskAuthenticationAuthorization.Models
     public class UserContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
 
         public UserContext(DbContextOptions<ShoppingContext> options)
             : base(options)
         {
             Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            string buyerRoleName = "byuer";
+
+            Role buyerRole = new Role { Id = 2, Name = buyerRoleName };
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
